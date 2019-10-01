e21dd9c30cb399dbd925e35c9ae843dfca292e85
Bohdan Kucheriavyi <bohdan.kucheriavy@ormdigital.com>
2019-10-01 12:32:35 +0300

    added challenge 2 implementation

---
+++ flizzbuzz.tests/GetWordFromTextChallenge.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Xunit;
+
+namespace flizzbuzz.tests
+{
+    public class WordFromTextChallengeTests
+    {
+        [Theory]
+        [InlineData("one two three", 2, "two")]
+        [InlineData("one;two three        ", 2, "three")]
+        [InlineData("     one;two three", 2, "three")]
+        [InlineData("     one;two three      ", 2, "three")]
+        [InlineData("one", 1, "one")]
+        [InlineData("one       ", 1, "one")]
+        [InlineData("     one", 1, "one")]
+        [InlineData("     one     ", 1, "one")]
+        public void GetWordFromText_ReturnsWordFromPos(string input, int pos, string expected)
+        {
+            //given
+            var chall = new WordFromTextChallenge();
+
+            //when
+            var result = chall.GetWordFromText(input, pos);
+
+            //then
+            Assert.Equal(expected, result);
+        }
+
+        [Theory]
+        [InlineData("one two three", -1)]
+        [InlineData("one;two three", 5)]
+        public void GetWordFromText_Throws(string input, int pos)
+        {
+            //given
+            var chall = new WordFromTextChallenge();
+            //when
+            //then
+            Assert.Throws<Exception>(() => chall.GetWordFromText(input, pos)); 
+            
+        }
+    }
+}
---
+++ flizzbuzz/WordFromTextChallenge.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+namespace flizzbuzz
+{
+    public class WordFromTextChallenge
+    {
+        public object GetWordFromText(string input, int pos)
+        {
+            if(pos < 1)
+            {
+                throw new Exception($"Invalid arg {nameof(pos)}");
+            }
+
+            var values = input.Trim().Split(' ').Select(c=> c.Trim()).ToArray();
+
+            if (pos > values.Length)
+            {
+                throw new Exception("Invalid position");
+            }
+
+            return values[pos - 1];
+        }
+    }
+}
