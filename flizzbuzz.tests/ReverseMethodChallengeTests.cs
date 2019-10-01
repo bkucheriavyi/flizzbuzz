42cea048c63953ddc8a2a5b009ebd2e39d68c24d
Bohdan Kucheriavyi <bohdan.kucheriavy@ormdigital.com>
2019-10-01 14:57:09 +0300

    Added Challange 3 impl

---
+++ flizzbuzz.tests/ReverseMethodChallengeTests.cs
@@ -0,0 +1,24 @@
+﻿using Xunit;
+
+namespace flizzbuzz.tests
+{
+    public class ReverseMethodChallengeTests
+    {
+        [Theory]
+        [InlineData("one", "eno")]
+        [InlineData("abcd", "dcba")]
+        [InlineData("", "")]
+        [InlineData(null, "")]
+        public void Reverse_GetsRevertedStringCorrectly(string input, string expected)
+        {
+            //given
+            var chall = new ReverseMethodChallenge();
+
+            //when
+            var restul = chall.Reverse(input);
+
+            //then
+            Assert.Equal(expected, restul);
+        }
+    }
+}
---
+++ flizzbuzz/ReverseMethodChallenge.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace flizzbuzz
+{
+    public class ReverseMethodChallenge
+    {
+        public string Reverse(string input)
+        {
+            if (input == null)
+            {
+                return string.Empty;
+            }
+
+            return new string(input.AsEnumerable().Reverse().ToArray());
+        }
+    }
+}
