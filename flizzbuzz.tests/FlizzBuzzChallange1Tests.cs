8370ae65e9f025d48efe15e79fa9be2129f31301
Bohdan Kucheriavyi <bohdan.kucheriavy@ormdigital.com>
2019-10-01 11:17:06 +0300

    added Step1 implementation

---
+++ flizzbuzz.tests/FlizzBuzzChallange1Tests.cs
@@ -0,0 +1,22 @@
+using Xunit;
+
+namespace flizzbuzz.tests
+{
+    public class FizzBuzzChallange1Tests
+    {
+        [Theory]
+        [InlineData(1, 20)]
+        public void FlizzBuzzChallange1(int from, int to)
+        {
+            //given
+            var expecetedOutput = "1 2 fizz 4 buzz fizz 7 8 fizz buzz 11 fizz 13 14 fizzbuzz 16 17 fizz 19 ";
+
+            //when
+            var flizBuz = new FizzBuzzChallange();
+
+            //then
+            var result = flizBuz.Magic_3_5_15(from, to);
+            Assert.Equal(expecetedOutput, result);
+        }
+    }
+}
---
+++ flizzbuzz/FlizzBuzzChallange.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Text;
+
+namespace flizzbuzz
+{
+    public class FizzBuzzChallange
+    {
+        public const string Fizz = "fizz";
+        public const string Buzz = "buzz";
+        public const string FizzBuzz = "fizzbuzz";
+
+        public string Magic_3_5_15(int from, int to)
+        {
+            if(from >= to)
+            {
+                throw new ArgumentException($"{nameof(from)} can't be >= {nameof(to)}");
+            }
+
+            if (from < 1 || to < 1)
+            {
+                throw new ArgumentException($"{nameof(from)},{nameof(to)} can't be less the 1");
+            }
+            var sb = new StringBuilder();
+            for (int i = 1; i <= to - from; i++)
+            {
+                if (i % 15 == 0)
+                {
+                    sb.Append(FizzBuzz);
+                    sb.Append(" ");
+                    continue;
+                }
+
+                if ( i % 5 == 0)
+                {
+                    sb.Append(Buzz);
+                    sb.Append(" ");
+                    continue;
+                }
+
+                if(i % 3 == 0)
+                {
+                    sb.Append(Fizz);
+                    sb.Append(" ");
+                    continue;
+                }
+
+                sb.Append(i);
+                sb.Append(" ");
+
+            }
+            return sb.ToString();
+        }
+    }
+}
